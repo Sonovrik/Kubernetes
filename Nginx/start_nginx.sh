3ba7879ca891de8b04223207a7ac34902bf161a3
Denis <lmidori@mi-h1.msk.21-school.ru>
2020-10-26 21:32:39 +0300

    step third

--- Nginx/start_nginx.sh
+++
@@ -1,5 +0,0 @@
-
-rc default
-
-# START NGINX
-nginx -g "daemon off;"
--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ eval $(minikube docker-env)
 
 docker build -t phpmyadmin PhpMyAdmin/
 docker build -t mysql SQL/
+docker build -t wordpress Wordpress/
+docker build -t nginx Nginx/
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
@@ -13,6 +15,8 @@ kubectl create secret generic -n metallb-system memberlist --from-literal=secret
 kubectl apply -f metallb-config.yaml
 
 kubectl apply -f PhpMyAdmin/phpmyadmin.yaml
+kubectl apply -f Wordpress/wordpress.yaml
 kubectl apply -f SQL/mysql.yaml
+kubectl apply -f Nginx/nginx.yaml
 
 minikube dashboard
