4e4bfa610e3bede904ba0f025c9269145aff573f
Denis <lmidori@mi-h1.msk.21-school.ru>
2020-10-27 23:06:44 +0300

    step fourth

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ docker build -t phpmyadmin PhpMyAdmin/
 docker build -t mysql SQL/
 docker build -t wordpress Wordpress/
 docker build -t nginx Nginx/
+docker build -t influxdb InfluxDB/
+docker build -t telegraf Telegraf/
+docker build -t ftps Ftp/
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
@@ -18,5 +21,8 @@ kubectl apply -f PhpMyAdmin/phpmyadmin.yaml
 kubectl apply -f Wordpress/wordpress.yaml
 kubectl apply -f SQL/mysql.yaml
 kubectl apply -f Nginx/nginx.yaml
+kubectl apply -f InfluxDB/influxdb.yaml
+kubectl apply -f Telegraf/telegraf.yaml
+kubectl apply -f Ftp/ftps.yaml
 
 minikube dashboard
